ead8876244041ee1eda780c350be72e101f8f6e1
HoGee <37961154+Hogeexxl@users.noreply.github.com>
2026-09-16 11:32:40 +0800

    style: apply rustfmt to Windows entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-#![cfg_attr(
-    all(target_os = "windows", not(test)),
-    windows_subsystem = "windows"
-)]
+#![cfg_attr(all(target_os = "windows", not(test)), windows_subsystem = "windows")]
 
 use std::sync::Arc;
 
